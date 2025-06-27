a13aa0d9b050a02ad55e4b740bff50218eb64502
daudulislamselise <daudul.islam@selise.ch>
2025-06-27 12:25:33 -0400

    Entity Added

---
+++ src/Swap.Posts.Domain/Entities/Posts.cs
@@ -0,0 +1,27 @@
+﻿using MongoDB.Bson;
+using MongoDB.Bson.Serialization.Attributes;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Swap.Posts.Domain.Entities
+{
+    public class Posts
+    {
+        [BsonId]
+        [BsonRepresentation(BsonType.String)]
+        public Guid ItemId { get; set; }
+        public string CreatedBy { get; set; }
+        public DateTime CreatedDate { get; set; } = DateTime.UtcNow;
+        public DateTime? LastUpdatedDate { get; set; }
+        public string LastUpdatedBy { get; set; }
+        public string Title { get; set; }
+        public string ImageUrl { get; set; }
+        public string Description { get; set; }
+        public bool IsArchive { get; set; }
+        public string CategoryName { get; set; }
+
+    }
+}
